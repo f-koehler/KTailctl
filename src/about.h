e58fc39531aad206c52b6edc1650980ae3a1f8d3
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-03-10 10:49:44 +0100

    Use include guards in headers from app template.

--- src/about.h
+++ src/about.h
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // SPDX-FileCopyrightText: 2023 Fabian Köhler <me@fkoehler.org>
 
-#pragma once
+#ifndef TAILCTL_ABOUT_H
+#define TAILCTL_ABOUT_H
 
-#include <QObject>
 #include <KAboutData>
+#include <QObject>
 
-class AboutType : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(KAboutData aboutData READ aboutData CONSTANT)
+class AboutType : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(KAboutData aboutData READ aboutData CONSTANT)
 public:
-    [[nodiscard]] KAboutData aboutData() const;
+  [[nodiscard]] KAboutData aboutData() const;
 };
+
+#endif /* TAILCTL_ABOUT_H */
--- src/app.h
+++ src/app.h
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // SPDX-FileCopyrightText: 2023 Fabian Köhler <me@fkoehler.org>
 
-#pragma once
+#ifndef TAILCTL_APP_H
+#define TAILCTL_APP_H
 
 #include <QObject>
 
 class QQuickWindow;
 
-class App : public QObject
-{
-    Q_OBJECT
+class App : public QObject {
+  Q_OBJECT
 
 public:
-    // Restore current window geometry
-    Q_INVOKABLE void restoreWindowGeometry(QQuickWindow *window, const QString &group = QStringLiteral("main")) const;
-    // Save current window geometry
-    Q_INVOKABLE void saveWindowGeometry(QQuickWindow *window, const QString &group = QStringLiteral("main")) const;
+  // Restore current window geometry
+  Q_INVOKABLE void
+  restoreWindowGeometry(QQuickWindow *window,
+                        const QString &group = QStringLiteral("main")) const;
+  // Save current window geometry
+  Q_INVOKABLE void
+  saveWindowGeometry(QQuickWindow *window,
+                     const QString &group = QStringLiteral("main")) const;
 };
+
+#endif /* TAILCTL_APP_H */
