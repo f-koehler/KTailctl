50d20f7d1ea9051825be78e67af123e92e28e92c
Fabian Köhler <me@fkoehler.org>
2023-09-15 19:43:05 +0200

    Remove deprecated files.

--- tailwrap/logging/go.mod
+++
@@ -1,3 +0,0 @@
-module tailwrap/logging
-
-go 1.21.0
--- tailwrap/logging/logging.h
+++
@@ -1,9 +0,0 @@
-#ifndef KTAILCTL_LOGGING_LOGGING_H
-#define KTAILCTL_LOGGING_LOGGING_H
-
-extern void ktailctl_critical(const char *message);
-extern void ktailctl_debug(const char *message);
-extern void ktailctl_info(const char *message);
-extern void ktailctl_warning(const char *message);
-
-#endif /* KTAILCTL_LOGGING_LOGGING_H */
