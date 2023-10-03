9f3e7bf51288c8a8d19ef2e6439b004f6f285600
Fabian Köhler <me@fkoehler.org>
2023-10-03 20:24:03 +0200

    Fix integer literal suffixes in util functions on 32bit platforms.

--- src/util.cpp
+++ src/util.cpp
@@ -39,7 +39,7 @@ QIcon loadOsIcon(QString osname)
 QString formatCapacityHumanReadable(long bytes)
 {
     static constexpr std::array<const char *, 5> units = {"TiB", "GiB", "MiB", "KiB", "B"};
-    static constexpr std::array<long, 5> limits = {1L << 40, 1L << 30, 1L << 20, 1L << 10, 1L};
+    static constexpr std::array<long, 5> limits = {1ll << 40ll, 1ll << 30ll, 1ll << 20ll, 1ll << 10ll, 1ll};
     for (std::size_t i = 0; i < units.size(); ++i) {
         if (bytes >= limits.at(i)) {
             return QString("%1 %2").arg(static_cast<double>(bytes) / static_cast<double>(limits.at(i)), 0, 'f', 3).arg(units.at(i));
@@ -51,10 +51,10 @@ QString formatCapacityHumanReadable(long bytes)
 QString formatSpeedHumanReadable(double bytes_per_second)
 {
     static constexpr std::array<const char *, 5> units = {"TiB/s", "GiB/s", "MiB/s", "KiB/s", "B/s"};
-    static constexpr std::array<double, 5> limits = {static_cast<double>(1L << 40),
-                                                     static_cast<double>(1L << 30),
-                                                     static_cast<double>(1L << 20),
-                                                     static_cast<double>(1L << 10),
+    static constexpr std::array<double, 5> limits = {static_cast<double>(1ll << 40ll),
+                                                     static_cast<double>(1ll << 30ll),
+                                                     static_cast<double>(1ll << 20ll),
+                                                     static_cast<double>(1ll << 10ll),
                                                      1.0};
     for (std::size_t i = 0; i < units.size(); ++i) {
         if (bytes_per_second >= limits.at(i)) {
@@ -66,8 +66,12 @@ QString formatSpeedHumanReadable(double bytes_per_second)
 
 QString formatDurationHumanReadable(const QDateTime &from, const QDateTime &to)
 {
-    static constexpr std::array<qint64, 6> conversions =
-        {365L * 30L * 24L * 60L * 60L * 1000L, 34 * 30L * 60L * 60L * 1000L, 24L * 60L * 60L * 1000L, 60L * 60L * 1000L, 60L * 1000L, 1000L};
+    static constexpr std::array<qint64, 6> conversions = {365ll * 30ll * 24ll * 60ll * 60ll * 1000ll,
+                                                          34 * 30ll * 60ll * 60ll * 1000ll,
+                                                          24ll * 60ll * 60ll * 1000ll,
+                                                          60ll * 60ll * 1000ll,
+                                                          60ll * 1000ll,
+                                                          1000ll};
     static constexpr std::array<const char *, 6> units = {"year", "month", "day", "hour", "minute", "second"};
 
     QString result = "";
