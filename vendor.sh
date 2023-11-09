c5bb5c99aba84370d300da2c850493023844f170
Fabian Köhler <me@fkoehler.org>
2023-11-09 09:59:43 +0800

    Do not delete vendor directory.

--- vendor.sh
+++ vendor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd tailwrap
+rm -rf vendor
 go mod vendor -v
 VERSION=$(git describe --tags --match "v*" --dirty)
 tar cfvz tailwrap-vendor-${VERSION}.tar.gz vendor
 mv tailwrap-vendor-${VERSION}.tar.gz ..
-rm -rf vendor
