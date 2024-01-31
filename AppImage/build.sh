2e808c1bd41592710a267c82bd7d78f6252b9bd4
Fabian Köhler <me@fkoehler.org>
2024-01-31 17:30:52 +0800

    Fix path of AppImage builder.

--- AppImage/build.sh
+++ AppImage/build.sh
@@ -3,4 +3,4 @@ mkdir -p build-AppImage AppDir
 PATH="/usr/local/go/bin:${PATH}" cmake -B build-AppImage -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr ..
 cmake --build build-AppImage
 make -C build-AppImage install DESTDIR=${PWD}/AppDir
-/usr/local/bin/appimage-builder
+./appimage-builder
