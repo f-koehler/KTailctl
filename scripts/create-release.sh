cb1dc1a799a5140712a7f6f3dabae656486c7579
Fabian Köhler <me@fkoehler.org>
2024-02-18 02:07:55 +0800

    Attach go vendor tarball and AppImage to release.

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -3,4 +3,16 @@
 TAG=$(git describe --tags --abbrev=0)
 echo "Create release ${TAG}"
 
+# Create release
 gh release create -F changelog/${TAG}.md --verify-tag ${TAG}
+
+# Attach vendored go dependencies
+./scripts/vendor.sh
+gh release upload ${TAG} tailwrap-vendor-${TAG}.tar.gz
+
+# Create an AppImage and attach it to the release
+yq '.AppDir.app_info.version = "${TAG}"' AppImage/AppImageBuilder.yml
+pushd AppImage
+CC=clang-17 CXX=clang++-17 ./build.sh
+gh release upload ${TAG} KTailctl-${TAG}-x86_64.AppImage
+popd
