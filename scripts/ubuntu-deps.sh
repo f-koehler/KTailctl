6d23d6bccf52064fd3b31dd2426eed5f49c610cf
Fabian Köhler <me@fkoehler.org>
2024-02-26 10:01:59 +0800

    Update list of ubuntu deps.

--- scripts/ubuntu-deps.sh
+++ scripts/ubuntu-deps.sh
@@ -3,22 +3,31 @@ set -euf -o pipefail
 
 PACKAGES=(
     appstream
+    argagg-dev
+    automake
+    autotools-dev
+    build-essential
     cmake
     extra-cmake-modules
+    fuse
     gettext
     kirigami2-dev
+    libboost-all-dev
+    libfuse-dev
+    libgpgme-dev
     libkf5config-dev
     libkf5coreaddons-dev
     libkf5guiaddons-dev
     libkf5i18n-dev
     libkf5notifications-dev
     libqt5svg5-dev
+    librsvg2-dev
+    libtool
+    nlohmann-json3-dev
+    pkg-config
     qtbase5-dev
     qtdeclarative5-dev
     qtquickcontrols2-5-dev
-    pkg-config
-    build-essential
-    fuse
 )
 
 apt-get update -y
