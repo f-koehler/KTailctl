3dde003d3083bc77d03641dc980481170ebe45a0
Fabian Köhler <me@fkoehler.org>
2024-03-01 17:49:11 +0800

    Add KDE Neon apt repo in GHA.

--- scripts/neon-deps.sh
+++ scripts/neon-deps.sh
@@ -7,15 +7,15 @@ PACKAGES=(
     extra-cmake-modules
     gettext
     kirigami2-dev
-    libkf5config-dev
-    libkf5coreaddons-dev
-    libkf5guiaddons-dev
-    libkf5i18n-dev
-    libkf5notifications-dev
-    libqt5svg5-dev
-    qtbase5-dev
-    qtdeclarative5-dev
-    qtquickcontrols2-5-dev
+    libkf6config-dev
+    libkf6coreaddons-dev
+    libkf6guiaddons-dev
+    libkf6i18n-dev
+    libkf6notifications-dev
+    libqt6svg6-dev
+    qtbase6-dev
+    qtdeclarative6-dev
+    qtquickcontrols2-6-dev
     pkg-config
     build-essential
     fuse
