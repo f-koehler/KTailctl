1c247ffc91fac8be1bf89abd7490d750b526bf6b
Fabian Köhler <me@fkoehler.org>
2024-03-01 16:41:01 +0800

    Update build system for KF6/Qt6. Fixes #151

--- scripts/lib/goqtflags.sh
+++ scripts/lib/goqtflags.sh
@@ -1,2 +1,2 @@
-export CGO_CXXFLAGS=$(pkg-config --cflags Qt5Core)
-export CGO_LDFLAGS=$(pkg-config --libs Qt5Core)
+export CGO_CXXFLAGS=$(pkg-config --cflags Qt6Core)
+export CGO_LDFLAGS=$(pkg-config --libs Qt6Core)
