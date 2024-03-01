514f791da4c78fa26ddc71e665d437b32a96224b
Fabian Köhler <me@fkoehler.org>
2024-03-01 17:52:58 +0800

    Try to fix apt update for Neon on GHA.

--- scripts/add-neon-apt-repo.sh
+++ scripts/add-neon-apt-repo.sh
@@ -3,4 +3,4 @@ set -euf -o pipefail
 
 apt-add-repository 'deb http://archive.neon.kde.org/user jammy main'
 wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
-apt update
+apt update -y
