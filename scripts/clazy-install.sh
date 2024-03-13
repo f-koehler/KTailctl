1f3725a0b3055a9d08a1c462ab63fdda27fb5a6f
Fabian Köhler <me@fkoehler.org>
2024-03-14 02:23:34 +0800

    Run clazy installation using sudo in GHA.

--- scripts/clazy-install.sh
+++ scripts/clazy-install.sh
@@ -4,5 +4,5 @@ set -euf -o pipefail
 git clone https://github.com/KDE/clazy
 cd clazy
 cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build
-cmake --build build --target install
+cmake --build build
 sudo cmake --build build --target install
