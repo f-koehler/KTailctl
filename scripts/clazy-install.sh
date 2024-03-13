aa18f58820d420d57641520a04f74ebdc106cf4e
Fabian Köhler <me@fkoehler.org>
2024-03-14 02:05:18 +0800

    Try using default ubuntu clang/llvm in clazy GHA.

--- scripts/clazy-install.sh
+++ scripts/clazy-install.sh
@@ -3,6 +3,6 @@ set -euf -o pipefail
 
 git clone https://github.com/KDE/clazy
 cd clazy
-cmake -DCMAKE_CXX_COMPILER=clang++-17 -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build
+cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build
 cmake --build build --target install
 sudo cmake --build build --target install
