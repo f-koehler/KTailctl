55a142f43b09d7be39c62e159215b212dc91b55e
Fabian Köhler <me@fkoehler.org>
2024-03-14 09:45:32 +0800

    Try clazy 1.11 in GHA.

--- scripts/clazy-install.sh
+++ scripts/clazy-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euf -o pipefail
 
-git clone https://github.com/KDE/clazy
+git clone https://github.com/KDE/clazy -b v1.11
 cd clazy
 cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build
 cmake --build build
