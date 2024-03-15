eae5503310fb3ee708bffbd0363b151c5be30701
Fabian Köhler <me@fkoehler.org>
2024-03-15 19:38:13 +0800

    Try to clazy via nix.

--- scripts/clazy-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euf -o pipefail
-
-git clone https://github.com/KDE/clazy -b v1.11
-cd clazy
-cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -B build
-cmake --build build
-sudo cmake --build build --target install
