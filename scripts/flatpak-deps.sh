bcaa147c04eb2b2abdaff4c32c8b582242bd75ec
Fabian Köhler <me@fkoehler.org>
2024-06-23 00:50:52 +0800

    Update kde runtime to version 6.7.

--- scripts/flatpak-deps.sh
+++ scripts/flatpak-deps.sh
@@ -3,6 +3,6 @@ set -euf -o pipefail
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub org.flatpak.Builder
 flatpak install --user -y flathub org.flathub.flatpak-external-data-checker
-flatpak install --user -y flathub org.kde.Sdk//6.6 org.kde.Platform/x86_64/6.6
+flatpak install --user -y flathub org.kde.Sdk//6.7 org.kde.Platform/x86_64/6.7
 flatpak install --user -y flathub org.freedesktop.Sdk.Extension.golang/x86_64/23.08
 flatpak install --user -y flathub runtime/org.freedesktop.Sdk.Extension.llvm17/x86_64/23.08
