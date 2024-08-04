293edd2b502c30d26831b045e68baab6ff215356
Fabian Köhler <me@fkoehler.org>
2024-08-04 21:26:38 +0800

    Update vendorHash in flake.

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
               pname = "ktailctl_wrapper";
               inherit src version;
               modRoot = "src/wrapper";
-              vendorHash = "sha256-Ls4MVppMJbUUukaKkDAN8Lx/s09JRJTf/RMgk0iDcnw=";
+              vendorHash = "sha256-V4Bn5/VaoFOZlNGBedA4Ly8Kocw0BWyfIHv8IU6Eay4=";
             })
             .goModules;
 
