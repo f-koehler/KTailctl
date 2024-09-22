53e0e68bddebac14ec859119aeef6694ff215923
Fabian Köhler <me@fkoehler.org>
2024-09-22 15:56:39 +0800

    Fix SDK versions for flatpak deps.

--- scripts/flatpak-deps.sh
+++ scripts/flatpak-deps.sh
@@ -4,5 +4,5 @@ flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/fl
 flatpak install --user -y flathub org.flatpak.Builder
 flatpak install --user -y flathub org.flathub.flatpak-external-data-checker
 flatpak install --user -y flathub org.kde.Sdk//6.7 org.kde.Platform/x86_64/6.7
-flatpak install --user -y flathub org.freedesktop.Sdk.Extension.golang/x86_64/24.08
-flatpak install --user -y flathub runtime/org.freedesktop.Sdk.Extension.llvm18/x86_64/24.08
+flatpak install --user -y flathub org.freedesktop.Sdk.Extension.golang/x86_64/23.08
+flatpak install --user -y flathub runtime/org.freedesktop.Sdk.Extension.llvm18/x86_64/23.08
