f35b32f0f7755fe9c7a4df1a449158151d3b084a
Fabian Koehler <fabian.koehler@proton.me>
2025-11-29 16:25:46 +0800

    fix(ci): try to fix flatpak build issue in CI

--- scripts/flatpak-deps.sh
+++ scripts/flatpak-deps.sh
@@ -2,6 +2,7 @@
 set -euf -o pipefail
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub org.freedesktop.Sdk.Extension.golang/x86_64/25.08
+flatpak install --user -y flathub org.freedesktop.Sdk.Extension.golang/x86_64/6.9
 flatpak install --user -y flathub org.flatpak.Builder
 flatpak install --user -y flathub org.flathub.flatpak-external-data-checker
 flatpak install --user -y flathub org.kde.Sdk//6.9 org.kde.Platform/x86_64/6.9
