1d3ebeadf317b88c15ec03c455f4c28e0cf16517
Fabian Koehler <fabian@fkoehler.me>
2026-03-02 18:32:06 +0800

    fix(tray_icon): properly implement tailscale toggling

--- src_new/tray_icon/tray_icon.hpp
+++ src_new/tray_icon/tray_icon.hpp
@@ -20,7 +20,8 @@ private:
     TrayMenuPeers *mMenuPeers;
     TrayMenuExitNodes *mMenuExitNodes;
     QAction *mActionShow;
-    QAction *mActionToggle;
+    QAction *mActionStart;
+    QAction *mActionStop;
     QAction *mActionQuit;
 
 signals:
@@ -29,14 +30,16 @@ signals:
 
 public:
     explicit TrayIcon(TailscaleNew *tailscale, QObject *parent = nullptr)
-        : QSystemTrayIcon(QIcon::fromTheme(QStringLiteral("unknown")), parent), mTailscale(tailscale)
+        : QSystemTrayIcon(QIcon::fromTheme(QStringLiteral("unknown")), parent)
+        , mTailscale(tailscale)
         , mContextMenu(new QMenu(QStringLiteral("Tray Menu")))
         , mMenuAccounts(new TrayMenuAccounts())
         , mMenuSelf(new TrayMenuSelf(tailscale))
         , mMenuPeers(new TrayMenuPeers(tailscale))
         , mMenuExitNodes(new TrayMenuExitNodes(tailscale))
         , mActionShow(new QAction(QIcon::fromTheme(QStringLiteral("window")), QStringLiteral("Show")))
-        , mActionToggle(new QAction(QIcon::fromTheme(QStringLiteral("stop")), QStringLiteral("Stop")))
+        , mActionStart(new QAction(QIcon::fromTheme(QStringLiteral("media-playback-start")), QStringLiteral("Start")))
+        , mActionStop(new QAction(QIcon::fromTheme(QStringLiteral("process-stop")), QStringLiteral("Stop")))
         , mActionQuit(new QAction(QIcon::fromTheme(QStringLiteral("application-exit")), QStringLiteral("Quit")))
     {
         setContextMenu(mContextMenu);
@@ -46,15 +49,19 @@ public:
         mContextMenu->addMenu(mMenuSelf);
         mContextMenu->addMenu(mMenuPeers);
         mContextMenu->addMenu(mMenuExitNodes);
-        mContextMenu->addAction(mActionToggle);
+        mContextMenu->addAction(mActionStart);
+        mContextMenu->addAction(mActionStop);
         mContextMenu->addSeparator();
         mContextMenu->addAction(mActionQuit);
 
         connect(mActionShow, &QAction::triggered, this, [this] {
             emit showWindow();
         });
-        connect(mActionToggle, &QAction::triggered, this, [this] {
-            mTailscale->toggle();
+        connect(mActionStart, &QAction::triggered, this, [this] {
+            mTailscale->up();
+        });
+        connect(mActionStop, &QAction::triggered, this, [this] {
+            mTailscale->down();
         });
         connect(mActionQuit, &QAction::triggered, this, [this] {
             emit quitRequested();
@@ -63,6 +70,16 @@ public:
         connect(mContextMenu, &QMenu::aboutToShow, mMenuSelf, &TrayMenuSelf::rebuildMenu);
         connect(mContextMenu, &QMenu::aboutToShow, mMenuPeers, &TrayMenuPeers::rebuildMenu);
         connect(mContextMenu, &QMenu::aboutToShow, mMenuExitNodes, &TrayMenuExitNodes::rebuildMenu);
+        connect(mContextMenu, &QMenu::aboutToShow, [this]() {
+            if ((mTailscale->status()->backendState() == Status::BackendState::Starting)
+                || (mTailscale->status()->backendState() == Status::BackendState::Running)) {
+                mActionStart->setVisible(false);
+                mActionStop->setVisible(true);
+            } else {
+                mActionStart->setVisible(true);
+                mActionStop->setVisible(false);
+            }
+        });
 
         connect(Config::self(), &Config::enableTrayIconChanged, [this]() {
             setVisible(Config::enableTrayIcon());
