58e87ce889714d54c6193e20e1cf888fdbbef753
Fabian Koehler <fabian@fkoehler.me>
2026-04-27 00:03:33 +0800

    fix(scripts): fix some linter diagnostics with flag download script

--- scripts/download-flags.py
+++ scripts/download-flags.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env -S uv run
-import pycountry
-import unicodedata
-import urllib.request
-import urllib.error
-from pathlib import Path
-import logging
-from rich.progress import Progress
-from rich.logging import RichHandler
 import multiprocessing
+import unicodedata
+import urllib.error
+import urllib.request
+from pathlib import Path
+
 import jinja2
+import pycountry
 
 
 def download_flag(country_code: str) -> Path | None:
@@ -17,19 +15,19 @@ def download_flag(country_code: str) -> Path | None:
     hex1 = hex(ord(symbol1)).lstrip("0x")
     hex2 = hex(ord(symbol2)).lstrip("0x")
     url = f"https://raw.githubusercontent.com/jdecked/twemoji/master/assets/svg/{hex1}-{hex2}.svg"
-    logging.info("Downloading %s", url)
+    print(f"Downloading {url}")
     target = Path.cwd() / "src" / "country-flags" / f"{country_code}.svg"
     target.parent.mkdir(parents=True, exist_ok=True)
     try:
-        urllib.request.urlretrieve(url, target)
+        _ = urllib.request.urlretrieve(url, target)
         return target
     except urllib.error.HTTPError:
-        logging.error("Failed to download: %s", url)
+        raise RuntimeError(f"Failed to download: {url}")
 
 
 def main():
     country_codes = [country.alpha_2.lower() for country in pycountry.countries]
-    flag_files: list[str] = []
+    flag_files: list[Path] = []
     with multiprocessing.Pool() as pool:
         for result in pool.imap(download_flag, country_codes):
             if result:
@@ -37,7 +35,7 @@ def main():
 
     flags = [file.stem for file in flag_files]
 
-    (Path.cwd() / "src" / "CountryFlags.cmake").write_text(
+    _ = (Path.cwd() / "src" / "CountryFlags.cmake").write_text(
         jinja2.Environment(
             loader=jinja2.FileSystemLoader(Path().cwd() / "src"),
         )
@@ -47,7 +45,4 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
-    )
     main()
