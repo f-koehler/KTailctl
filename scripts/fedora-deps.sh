0fd4cfc9fddec60849610b1fb7a06b0d28e59d97
Fabian Koehler <fabian@fkoehler.me>
2026-04-27 00:09:08 +0800

    fix(scripts): cleanup fedora-deps.sh/neon-deps.sh

--- scripts/fedora-deps.sh
+++ scripts/fedora-deps.sh
@@ -2,22 +2,22 @@
 set -euf -o pipefail
 
 PACKAGES=(
-    extra-cmake-modules
-    qt6-qtbase-devel
-    qt6-qtdeclarative-devel
-    qt6-qtquickcontrols2-devel
-    qt6-qtsvg-devel
-    kf6-kconfig-devel
-    kf6-kcoreaddons-devel
-    kf6-kguiaddons-devel
-    kf6-ki18n-devel
-    kf6-kirigami2-devel
-    kf6-knotifications-devel
-    kf6-kdbusaddons-devel
-    kf6-kwindowsystem-devel
-    kf6-breeze-icons-devel
-    json-devel
-    go
+	extra-cmake-modules
+	qt6-qtbase-devel
+	qt6-qtdeclarative-devel
+	qt6-qtquickcontrols2-devel
+	qt6-qtsvg-devel
+	kf6-kconfig-devel
+	kf6-kcoreaddons-devel
+	kf6-kguiaddons-devel
+	kf6-ki18n-devel
+	kf6-kirigami2-devel
+	kf6-knotifications-devel
+	kf6-kdbusaddons-devel
+	kf6-kwindowsystem-devel
+	kf6-breeze-icons-devel
+	json-devel
+	go
 )
 
 dnf install -y "${PACKAGES[@]}"
--- scripts/neon-deps.sh
+++ scripts/neon-deps.sh
@@ -2,28 +2,29 @@
 set -euf -o pipefail
 
 PACKAGES=(
-    appstream
-    build-essential
-    cmake
-    extra-cmake-modules
-    fuse
-    gettext
-    kf6-kirigami-addons-dev
-    kf6-kirigami-dev
-    libkf6config-dev
-    libkf6coreaddons-dev
-    libkf6guiaddons-dev
-    libkf6i18n-dev
-    libkf6notifications-dev
-    libkf6notifications-dev
-    libkf6windowsystem-dev
-    libkf6dbusaddons-dev
-    libqt6quickcontrols2-6
-    libqt6svg6-dev
-    pkg-config
-    qt6-base-dev
-    qt6-declarative-dev
-    qml6-module-org-kde-kirigami-addons-delegates
+	appstream
+	build-essential
+	clang
+	cmake
+	extra-cmake-modules
+	gettext
+	kf6-breeze-icons
+	kf6-kirigami-addons-dev
+	kf6-kirigami-dev
+	libkf6config-dev
+	libkf6coreaddons-dev
+	libkf6dbusaddons-dev
+	libkf6guiaddons-dev
+	libkf6i18n-dev
+	libkf6notifications-dev
+	libkf6windowsystem-dev
+	libqt6quickcontrols2-6
+	libqt6svg6-dev
+	ninja-build
+	pkg-config
+	qt6-base-dev
+	qt6-declarative-dev
+	qml6-module-org-kde-kirigami-addons-delegates
 )
 
 apt-get update -y
