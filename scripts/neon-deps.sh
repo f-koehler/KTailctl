6407a482519131e447fd6af6cc0d496379f7d333
Fabian Koehler <fabian@fkoehler.me>
2026-05-03 23:25:31 +0800

    fix(scripts): fix KDE neon deps script

--- scripts/neon-deps.sh
+++ scripts/neon-deps.sh
@@ -8,7 +8,7 @@ PACKAGES=(
 	cmake
 	extra-cmake-modules
 	gettext
-	kf6-breeze-icons
+	kf6-breeze-icon-theme-dev
 	kf6-kirigami-addons-dev
 	kf6-kirigami-dev
 	libkf6config-dev
@@ -22,9 +22,9 @@ PACKAGES=(
 	libqt6svg6-dev
 	ninja-build
 	pkg-config
+	qml6-module-org-kde-kirigami-addons-delegates
 	qt6-base-dev
 	qt6-declarative-dev
-	qml6-module-org-kde-kirigami-addons-delegates
 )
 
 apt-get update -y
