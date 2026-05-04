796ed0e24871c8711f5b8ade732beb85db0aa1e3
Fabian Koehler <fabian@fkoehler.me>
2026-05-05 00:38:39 +0800

    feat(tray): work on building out accounts tray menu

--- src/tray_icon/tray_menu_accounts.hpp
+++ src/tray_icon/tray_menu_accounts.hpp
@@ -1,16 +1,46 @@
 #ifndef KTAILCTL_TRAY_MENU_ACCOUNTS_HPP
 #define KTAILCTL_TRAY_MENU_ACCOUNTS_HPP
+#include "logging_tray_icon.hpp"
+#include "tailscale.hpp"
 #include <QMenu>
+#include <QModelIndex>
 
 class TrayMenuAccounts : public QMenu
 {
     Q_OBJECT
 
+    Tailscale *mTailscale;
+    bool mRoleIndicesFound = false;
+    int mRoleIndexName = -1;
+
 public:
-    explicit TrayMenuAccounts(QWidget *parent = nullptr)
+    explicit TrayMenuAccounts(Tailscale *tailscale, QWidget *parent = nullptr)
         : QMenu(QStringLiteral("Accounts"), parent)
+        , mTailscale(tailscale)
+        , mRoleIndexName(tailscale->loginProfileModel()->roleIndexForProperty("name"))
     {
         setIcon(QIcon::fromTheme(QStringLiteral("config-users")));
+        if (mRoleIndexName == -1) {
+            qCCritical(Logging::TrayIcon) << "Failed to find role index for name";
+            mRoleIndicesFound = false;
+        }
+
+        connect(this, &QMenu::aboutToShow, this, &TrayMenuAccounts::rebuildMenu);
+    }
+
+public Q_SLOTS:
+    void rebuildMenu()
+    {
+        clear();
+        if (!mRoleIndicesFound) {
+            return;
+        }
+        const int rows = mTailscale->loginProfileModel()->rowCount(QModelIndex{});
+        for (int row = 0; row < rows; ++row) {
+            const QModelIndex index = mTailscale->loginProfileModel()->index(row, 0);
+            QAction *action = addAction(mTailscale->loginProfileModel()->data(index, mRoleIndexName).toString());
+            action->setCheckable(true);
+        }
     }
 };
 
