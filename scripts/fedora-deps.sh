facd4e35b704db771fa58cef32dacf0ec8253322
Fabian Koehler <fabian@fkoehler.me>
2026-05-16 14:05:15 +0800

    feat(ci): integrate clang-tidy and clazy

--- scripts/fedora-deps.sh
+++ scripts/fedora-deps.sh
@@ -2,21 +2,23 @@
 set -euf -o pipefail
 
 PACKAGES=(
+	clang-tools-extra
+	clazy
 	extra-cmake-modules
-	qt6-qtbase-devel
-	qt6-qtdeclarative-devel
-	qt6-qtquickcontrols2-devel
-	qt6-qtsvg-devel
+	go
+	kf6-breeze-icons-devel
 	kf6-kconfig-devel
 	kf6-kcoreaddons-devel
+	kf6-kdbusaddons-devel
 	kf6-kguiaddons-devel
 	kf6-ki18n-devel
 	kf6-kirigami2-devel
 	kf6-knotifications-devel
-	kf6-kdbusaddons-devel
 	kf6-kwindowsystem-devel
-	kf6-breeze-icons-devel
-	go
+	qt6-qtbase-devel
+	qt6-qtdeclarative-devel
+	qt6-qtquickcontrols2-devel
+	qt6-qtsvg-devel
 )
 
 dnf install -y "${PACKAGES[@]}"
