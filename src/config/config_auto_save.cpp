2a5de5fed17e116ab66aa5f62288f60a6faf3b3a
Fabian Koehler <fabian@fkoehler.me>
2026-05-24 17:23:38 +0800

    fix: fix some SonarQube reports around config auto save

--- src/config/config_auto_save.cpp
+++ src/config/config_auto_save.cpp
@@ -6,7 +6,7 @@ ConfigAutoSave::ConfigAutoSave(Config *config, QObject *parent)
     : QObject(parent)
 {
     const QMetaObject *metaObj = config->metaObject();
-    const QMetaMethod saveSlot = metaObject()->method(metaObject()->indexOfSlot("save()"));
+    const QMetaMethod saveSlot = ConfigAutoSave::staticMetaObject.method(ConfigAutoSave::staticMetaObject.indexOfSlot("save()"));
     for (int i = metaObj->methodOffset(); i < metaObj->methodCount(); ++i) {
         const QMetaMethod method = metaObj->method(i);
         if (method.methodType() == QMetaMethod::Signal && method.name().endsWith("Changed")) {
