b4f9f1ecb8dd50cb2db794db0d0a4143301c33d2
Fabian Koehler <fabian@fkoehler.me>
2026-05-28 00:32:54 +0800

    feat(ci): support creating of beta releases

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -3,8 +3,18 @@
 TAG=$(git describe --tags --abbrev=0)
 echo "Create release ${TAG}"
 
-# Create release
-gh release create -F "changelog/${TAG}.md"--verify-tag "${TAG}"
+if [[ "${TAG}" =~ -beta[0-9]+$ ]]; then
+	gh release create \
+		--prerelease \
+		--verify-tag \
+		--notes "This is a pre-release build for testing purposes only. It may contain bugs and is not intended for production use." \
+		"${TAG}"
+else
+	gh release create \
+		--verify-tag \
+		-F "changelog/${TAG}.md" \
+		"${TAG}"
+fi
 
 # Attach vendored go dependencies
 ./scripts/vendor.sh
