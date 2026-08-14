2e44283e57e63e5195ca6ea46b502d23a49fb864
Fabian Koehler <fabian@fkoehler.me>
2026-08-14 12:30:19 +0800

    chore(flatpak): update KDE SDK for local flatpak build

--- scripts/flatpak-deps.sh
+++ scripts/flatpak-deps.sh
@@ -3,4 +3,4 @@ set -euf -o pipefail
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub org.flatpak.Builder
 flatpak install --user -y flathub org.flathub.flatpak-external-data-checker
-flatpak install --user -y flathub org.kde.Sdk//6.10 org.kde.Platform/x86_64/6.10
+flatpak install --user -y flathub org.kde.Sdk//6.11 org.kde.Platform/x86_64/6.11
